d010fd3b098f98d3122939af01118a1827b8b4a5
Estrella0812 <88253146+Estrella0812@users.noreply.github.com>
2024-10-26 15:16:15 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 import Header from "./header/page";
 
 
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics/>
     </html>
   );
 }
